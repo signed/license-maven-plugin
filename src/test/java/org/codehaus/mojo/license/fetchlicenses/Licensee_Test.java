f413cd62c1c25d40217c97e99e58431333a299b4
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-07-05 10:10:34 +0200

    Restructured test

--- src/test/java/org/codehaus/mojo/license/fetchlicenses/Licensee_Test.java
+++ src/test/java/org/codehaus/mojo/license/fetchlicenses/Licensee_Test.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,16 +15,19 @@ public class Licensee_Test {
 
     @Rule
     public final TemporaryFolder thirdPartyLicenses = new TemporaryFolder();
+    private final GavCoordinates coordinates = new GavCoordinates("org.example", "artifact", "do not care");
+    private final Text licenseText = new Text("The license text");
 
     @Test
     public void putTheLicenseTextIntoTheThirdPartyLicenseDirectory() throws Exception {
-        GavCoordinates coordinates = new GavCoordinates("org.example", "artifact", "do not care");
-        Text licenseText = new Text("The license text");
-
         new Licensee(thirdPartyLicenses.getRoot()).complyWith(new LicenseObligations(coordinates, licenseText));
-        File root = thirdPartyLicenses.getRoot();
-        File file = new File(root, "org.example.artifact.LICENSE.txt");
 
-        assertThat(FileUtils.readFileToString(file, "UTF-8"), is("The license text"));
+        assertThat(contentOfFileInThirdPartyLicenses("org.example.artifact.LICENSE.txt"), is("The license text"));
+    }
+
+    private String contentOfFileInThirdPartyLicenses(String child) throws IOException {
+        File root = thirdPartyLicenses.getRoot();
+        File file = new File(root, child);
+        return FileUtils.readFileToString(file, "UTF-8");
     }
 }
