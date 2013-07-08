0b23121eddbbede2f3920a93f101f9dcf884e034
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-07-08 10:21:10 +0200

    remove duplication and other refactorings

--- src/main/java/org/codehaus/mojo/license/fetchlicenses/repository/VersionMappingParser.java
+++ src/main/java/org/codehaus/mojo/license/fetchlicenses/repository/VersionMappingParser.java
@@ -3,10 +3,12 @@ package org.codehaus.mojo.license.fetchlicenses.repository;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.String.format;
+
 public class VersionMappingParser {
     public static final String WellKnownLicenseProtocol = "well-known-license";
     public static final String SubDirectoryProtocol = "sub-directory";
-    public static final String lineFormatRegex = "(^" + WellKnownLicenseProtocol + "|" + SubDirectoryProtocol + ")://(.+)<-(.+)$";
+    public static final String lineFormatRegex = format("(^%s|%s)://(.+)<-(.+)$", WellKnownLicenseProtocol, SubDirectoryProtocol);
 
     private final static Pattern locationPattern = Pattern.compile("://([^\\s]+)\\s*<-");
     private final MappingBuilder builder;
@@ -20,19 +22,25 @@ public class VersionMappingParser {
             return;
         }
 
-        Matcher matcher = locationPattern.matcher(line);
-        matcher.find();
-
-        String location = matcher.group(1);
-        if (line.startsWith(WellKnownLicenseProtocol + "://")) {
-            builder.wellKnownLicense(location);
-        } else if (line.startsWith(SubDirectoryProtocol + "://")) {
-            builder.subDirectory(location);
+        if (line.startsWith(protocolHeaderFor(WellKnownLicenseProtocol))) {
+            builder.wellKnownLicense(extractTargetFrom(line));
+        } else if (line.startsWith(protocolHeaderFor(SubDirectoryProtocol))) {
+            builder.subDirectory(extractTargetFrom(line));
         }
 
         String[] versions = line.split("<-")[1].trim().split(",");
         for (String version : versions) {
             builder.version(version.trim());
         }
+    }
+
+    private String extractTargetFrom(String line) {
+        Matcher matcher = locationPattern.matcher(line);
+        matcher.find();
+        return matcher.group(1);
+    }
+
+    private String protocolHeaderFor(String protocol) {
+        return protocol + "://";
     }
 }
