56461a45e575e5ece5f83618484c827889e3b98f
Thomas Heilbronner <Thomas.Heilbronner@gmail.com>
2013-07-10 08:46:24 +0200

    publish the sub directory as public field

--- src/main/java/org/codehaus/mojo/license/fetchlicenses/repository/Target.java
+++ src/main/java/org/codehaus/mojo/license/fetchlicenses/repository/Target.java
@@ -5,8 +5,10 @@ import java.io.File;
 public class Target {
 
     public final File directory;
+    public final String subDirectory;
 
     public Target(File rootFolder, String subDirectory) {
+        this.subDirectory = subDirectory;
         directory = new File(rootFolder, subDirectory);
     }
 }
